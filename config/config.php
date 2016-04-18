f5200193fcd078629014cc07c6f2b0d3c711f3b0
root <root@static.148-251-70-116.clients.your-server.de>
2016-04-18 08:46:24 +0200

    added copyright informations and comments

--- config/config.php
+++ config/config.php
@@ -1,6 +1,18 @@
 <?php
 
+/**
+ * Content elements
+ **/
 array_insert($GLOBALS['TL_CTE']['media'], count($GLOBALS['TL_CTE']['media']), array
 (
 	'twentytwenty'  => 'ContentTwentyTwenty'
 ));
+
+/**
+ * Copyright information
+ */
+$GLOBALS['TL_HEAD']['PIXELSPREADDE'] = '<!--
+    This Contao OpenSource CMS uses modules from pixelSpread.de
+    Copyright (c)2012 - ' . date("Y") . ' by Sascha Brandhoff :: Extensions of pixelSpread.de are copyright of their respective owners
+    Visit our website at http://www.pixelSpread.de for more information
+//-->';
